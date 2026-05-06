4a581090990a3e57ea98197ead4801bede5ff9af
Geoffrey Miles <geoffreymiles054@gmail.com>
2026-05-06 03:24:37 -0500

    Added set_money function

--- source/rby-edit.c
+++ source/rby-edit.c
@@ -7,6 +7,7 @@
 #define CHECKSUM_END_ADDR 0x3522
 
 #define PLAYER_NAME_ADDR 0x2598
+#define MONEY_ADDR 0x25F3
 
 typedef struct
 {
@@ -29,6 +30,30 @@ void set_checksum(FILE* file)
 	buffer[0] = ~checksum;
 	fseek(file, CHECKSUM_ADDR, SEEK_SET);
 	fwrite(buffer, 1, 1, file);
+}
+
+void set_money(FILE *file, uint32_t amount)
+{
+	// Pokemon uses a Binary-coded decimal to store money. we need to convert to bcd then
+	// reverse the endian because Pokemon uses big endian. before we swap endian we shift
+	// the bytes left to get rid of the 32 bit hight byte since we only need the bottom 24.
+	
+	if(amount > 999999) amount = 999999;
+	
+	uint32_t bcd = 0;
+	int shift = 0;
+	while(amount > 0)
+	{
+		bcd |= (amount % 10) << shift;
+		amount /= 10;
+		shift += 4;
+	}
+
+	bcd = bcd << 8;
+	bcd = __builtin_bswap32(bcd);
+
+	fseek(file, MONEY_ADDR, SEEK_SET);
+	fwrite(&bcd, 3, 1, file);	
 }
 
 void convert_ASCII(String string)
@@ -68,6 +93,7 @@ int main(int argc, char *args[])
 
 	convert_ASCII(name);
 	write_string(file, PLAYER_NAME_ADDR, name);
+	set_money(file, 67420);
 
 	set_checksum(file); 
 	return 0;
